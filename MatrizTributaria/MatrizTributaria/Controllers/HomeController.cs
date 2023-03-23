c4e3ecda928a2a73160377158e595b798612be3d
desenvolvimentomtx <desenvolvimentomtx@gmail.com>
2023-03-23 09:22:04 -0300

    SEGUNDA ALTERAÇÃO

--- MatrizTributaria/MatrizTributaria/Controllers/HomeController.cs
+++ MatrizTributaria/MatrizTributaria/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ using System.Net.Mail;
 using System.Text.RegularExpressions;
 using System.Globalization;
 using System.Net.Mime;
-//SALVO alteração paulo
+//SALVO alteração paulo SEGUNDA ALTERAÇÃO
 namespace MatrizTributaria.Controllers
 {
     public class HomeController : Controller
