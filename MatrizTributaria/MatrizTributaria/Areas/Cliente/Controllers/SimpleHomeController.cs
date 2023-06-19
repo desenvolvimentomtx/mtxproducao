8f7ea51e4410257151b78db6aa5faef41f944cc5
horizonti <pnagi2012@gmail.com>
2023-06-19 17:59:12 -0300

    alteracoes

---
+++ MatrizTributaria/MatrizTributaria/Areas/Cliente/Controllers/SimpleHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MatrizTributaria.Areas.Cliente.Controllers
+{
+    public class SimpleHomeController : Controller
+    {
+        // GET: Cliente/SimpleHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
