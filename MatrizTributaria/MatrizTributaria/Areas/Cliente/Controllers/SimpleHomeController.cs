68d3583c3b771de830166b91783de3398481a645
horizonti <pnagi2012@gmail.com>
2023-06-20 17:53:42 -0300

    Alteraçãoes tela simples

--- MatrizTributaria/MatrizTributaria/Areas/Cliente/Controllers/SimpleHomeController.cs
+++ MatrizTributaria/MatrizTributaria/Areas/Cliente/Controllers/SimpleHomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MatrizTributaria.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,39 @@ namespace MatrizTributaria.Areas.Cliente.Controllers
 {
     public class SimpleHomeController : Controller
     {
+        readonly private MatrizDbContext db = new MatrizDbContext();
+        Empresa empresa;
         // GET: Cliente/SimpleHome
-        public ActionResult Index()
+        public ActionResult Index(string cnpj)
         {
+            //usar esse link
+            //https://localhost:44324/Cliente/SimpleHome/Index?cnpj=15202475000219
+
+            //formatando o cnpj
+            string cnpjRecebido = FormatCNPJ(cnpj); 
+    
+            //pegando a empresa
+            this.empresa = (from a in db.Empresas where a.cnpj == cnpjRecebido select a).FirstOrDefault(); 
+
+
+            //passando o nome fantasia para a view
+            ViewBag.Empresa = this.empresa.fantasia;
+
+            //pegando o estado da empresa
+            ViewBag.UfOrigem = this.empresa.estado;
+            ViewBag.UfDestino = this.empresa.estado;
+
+
+
+
+
             return View();
+        }
+
+
+        public static string FormatCNPJ(string CNPJ)
+        {
+            return Convert.ToUInt64(CNPJ).ToString(@"00\.000\.000\/0000\-00");
         }
     }
 }
